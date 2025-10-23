537d53422c0609f405a11c7e0eaf45551d4ef849
Constantin Kühne <constantin.kuehne@student.hpi.uni-potsdam.de>
2025-10-23 16:45:21 +0200

    implemented ingestion + tokenization

--- search_engine/ingestion.py
+++ search_engine/ingestion.py
@@ -6,12 +6,14 @@ def tokenize_text(text: str) -> list[str]:
     tokens = nltk.word_tokenize(text)
     return tokens
 
-
-if __name__ == "__main__":
-    with open("msmarco-docs.tsv", mode="r") as file:
+def process_csv(file_path: str):
+    with open(file_path, mode="r") as file:
         reader = csv.DictReader(
             file, delimiter="\t", fieldnames=["docid", "url", "title", "body"]
         )
         for row in reader:
             tokens = tokenize_text(row["body"])
             print(f"Document ID: {row['docid']}, Tokens: {tokens[:10]}...")
+
+if __name__ == "__main__":
+    process_csv("../msmarco.csv")
