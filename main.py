aea8392c020aa48fb25a962503ef3389b35d2c0c
Constantin Kühne <constantin.kuehne@student.hpi.uni-potsdam.de>
2025-10-28 17:13:34 +0100

    added support for search modes

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import time
 import search_engine
+import argparse
+
+from search_engine.inverted_index import SearchMode
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Simple Inverted Index Search Engine")
+    parser.add_argument(
+        "--mode",
+        type=SearchMode,
+        choices=[SearchMode.AND, SearchMode.OR, SearchMode.NOT, SearchMode.PHRASE],
+        default=SearchMode.AND,
+        help=f"Search mode: {SearchMode.AND} or {SearchMode.OR} or {SearchMode.NOT} or {SearchMode.PHRASE}",
+    )
+    args = parser.parse_args()
+    print(args.mode)
+
     print("Starting indexing...")
     index = search_engine.InvertedIndex()
-    for row in search_engine.ingestion.process_data(
-        "./msmarco-docs.tsv", max_rows=15_000
-    ):
+    for row in search_engine.ingestion.process_data("./msmarco-docs.tsv", max_rows=1_000):
         index.add_document(int(row.docid[1:]), row.url, row.title, row.tokens)
 
     print("Indexing complete.\n")
@@ -16,7 +28,7 @@ if __name__ == "__main__":
     start = time.time()
 
     num_return = 10
-    num_results, results = index.search(query, num_return=num_return)
+    num_results, results = index.search(query, mode=args.mode, num_return=num_return)
     print(f"We found {num_results} results matching your query.")
     print(f"{num_return} of them are:")
     for result in results:
--- search_engine/inverted_index.py
+++ search_engine/inverted_index.py
@@ -1,5 +1,6 @@
 from search_engine.preprocessing import tokenize_text
 from typing import NamedTuple
+from enum import Enum
 
 POSITIONS = dict[int, list[int]]
 
@@ -13,6 +14,13 @@ class SearchResult(NamedTuple):
     doc_id: int
     url: str
     title: str
+
+
+class SearchMode(Enum):
+    AND = "AND"
+    OR = "OR"
+    NOT = "NOT"
+    PHRASE = "PHRASE"
 
 
 class InvertedIndex:
@@ -39,8 +47,42 @@ class InvertedIndex:
 
                 self.index[term] = (doc_freq, position_list)
 
+    def has_phrase(self, doc_id: int, tokens: list[str]) -> bool:
+        pos_lists = []
+        for token in tokens:
+            pos_lists.append(self.index[token][1][doc_id])
+
+        indices = [0 for _ in range(len(pos_lists))]
+        print(indices)
+        has_phrase: bool = False
+
+        for _ in range(len(pos_lists[0])):
+            for i, pos_list in enumerate(pos_lists[1:]):
+                print(indices[i + 1])
+                print(f"{len(pos_list)=}")
+                while (
+                    pos_list[indices[i + 1]] < pos_lists[i][indices[i]]
+                ):  # +1 because we skip first list
+                    indices[i + 1] += 1
+
+                    if indices[i + 1] >= len(pos_list):
+                        return False
+
+                if pos_list[indices[i + 1]] == pos_lists[i][indices[i]] + 1:
+                    has_phrase = True
+                else:
+                    has_phrase = False
+                    break
+
+            if has_phrase:
+                break
+
+            indices[0] += 1
+
+        return has_phrase
+
     def search(
-        self, query: str, num_return: int = 10
+        self, query: str, mode: SearchMode, num_return: int = 10
     ) -> tuple[int, list[SearchResult]]:
         tokens = tokenize_text(query)
 
@@ -53,14 +95,34 @@ class InvertedIndex:
             else:
                 # add the empty set if term not found, so we give no results
                 # the correct AND semantic
-                doc_list.append(set()) 
+                doc_list.append(set())
 
-        if len(doc_list) == 0:
-            matched = []
-        elif len(doc_list) == 1:
-            matched = list(doc_list[0])
-        else:
-            matched = list(doc_list[0].intersection(*doc_list[1:]))
+        matched = []
+        if mode == SearchMode.AND:
+            if len(doc_list) == 1:
+                matched = list(doc_list[0])
+            elif len(doc_list) > 1:
+                matched = list(doc_list[0].intersection(*doc_list[1:]))
+        elif mode == SearchMode.OR:
+            if len(doc_list) == 1:
+                matched = list(doc_list[0])
+            elif len(doc_list) > 1:
+                matched = list(doc_list[0].union(*doc_list[1:]))
+        elif mode == SearchMode.NOT:
+            if len(doc_list) == 0:
+                matched = list(self.docs.keys())
+            else:
+                matched = list(set(self.docs.keys()).difference(*doc_list))
+        elif mode == SearchMode.PHRASE:
+            if len(doc_list) == 1:
+                print(doc_list)
+                matched = list(doc_list[0])
+            elif len(doc_list) > 1:
+                match_candidates = doc_list[0].intersection(*doc_list[1:])
+                matched = []
+                for doc_id in match_candidates:
+                    if self.has_phrase(doc_id, tokens):
+                        matched.append(doc_id)
 
         results = [
             SearchResult(
