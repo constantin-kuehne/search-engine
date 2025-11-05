b0c1458b6eb996013e5d853eba519e8d5437f096
Constantin Kühne <constantin.kuehne@student.hpi.uni-potsdam.de>
2025-11-05 15:48:19 +0100

    More than 1 query per execution

--- main.py
+++ main.py
@@ -9,30 +9,43 @@ if __name__ == "__main__":
     parser.add_argument(
         "--mode",
         type=SearchMode,
-        choices=[SearchMode.AND, SearchMode.OR, SearchMode.NOT, SearchMode.PHRASE],
+        choices=[x for x in SearchMode],
         default=SearchMode.AND,
-        help=f"Search mode: {SearchMode.AND} or {SearchMode.OR} or {SearchMode.NOT} or {SearchMode.PHRASE}",
+        required=True,
+        help=f"Search mode: {' or '.join([str(x) for x in SearchMode])}",
+    )
+    parser.add_argument(
+        "--num_return",
+        type=int,
+        default=10,
+        help="How many search results should be returned"
     )
     args = parser.parse_args()
-    print(args.mode)
 
     print("Starting indexing...")
     index = search_engine.InvertedIndex()
-    for row in search_engine.ingestion.process_data("./msmarco-docs.tsv", max_rows=1_000):
+    for row in search_engine.ingestion.process_data(
+        "./msmarco-docs.tsv", max_rows=1_000
+    ):
         index.add_document(int(row.docid[1:]), row.url, row.title, row.tokens)
 
     print("Indexing complete.\n")
 
-    query = input("Enter your search query: ")
+    try:
+        while True:
+            query = input("Enter your search query: ")
 
-    start = time.time()
+            start = time.time()
 
-    num_return = 10
-    num_results, results = index.search(query, mode=args.mode, num_return=num_return)
-    print(f"We found {num_results} results matching your query.")
-    print(f"{num_return} of them are:")
-    for result in results:
-        print(f"DocId: {result.doc_id} ({result.url}) - {result.title}")
+            num_results, results = index.search(
+                query, mode=args.mode, num_return=args.num_return
+            )
+            print(f"We found {num_results} results matching your query.")
+            print(f"{args.num_return} of them are:")
+            for result in results:
+                print(f"DocId: {result.doc_id} ({result.url}) - {result.title}")
 
-    end = time.time()
-    print(f"\nSearch took {end - start:.4f} seconds.")
+            end = time.time()
+            print(f"\nSearch took {end - start:.4f} seconds.")
+    except KeyboardInterrupt:
+        print("\nprogram terminated")
